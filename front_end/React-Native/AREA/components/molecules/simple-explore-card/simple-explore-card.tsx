a398d0a96e43a9e0d8198b2e17442bedbb8af6a7
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-09-24 14:27:20 +0200

    feat(frontend/mobile/): add simple-explore-card componenet

---
+++ front_end/React-Native/AREA/components/molecules/simple-explore-card/simple-explore-card.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Image } from "expo-image";
+
+type SimpleExploreCardProps = {
+  appName: string;
+  backgroundColor: string;
+  appLogoPath: string | number;
+};
+
+export default function SimpleExploreCard({ appName, backgroundColor, appLogoPath }: SimpleExploreCardProps) { //TODO: modifier la simple explore card
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={{ height: 200}}
+        onPress={() => {
+        }}>
+        <View style={[styles.button, {backgroundColor}]}>
+          <Image style={styles.appLogo}
+            source={appLogoPath}
+          />
+          <Text style={styles.buttonText}>
+            {appName}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    marginVertical: 8,
+  },
+
+  button: {
+    alignItems: "center",
+    backgroundColor: "#e6e3e3ff",
+    borderRadius: 10,
+    padding: 12,
+    height: 200,
+  },
+
+  appLogo: {
+    width: 140,
+    height: 140,
+    // borderRadius: 8,
+  },
+
+  buttonText: {
+    flex: 1,
+    fontSize: 24,
+    color: "#eee",
+    fontWeight: "bold",
+  },
+});
