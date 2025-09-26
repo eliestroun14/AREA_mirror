a03da5535d29797ada5eabc880b41ffd5a17e19f
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2025-09-26 10:34:55 +0200

    feat(frontend/mobile/): create service-details page base

---
+++ front_end/React-Native/AREA/app/service-details/[id].tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {}
+
+const ServiceDetails = (props: Props) => {
+  return (
+    <View>
+      <Text>
+        Service Details
+      </Text>
+    </View>
+  )
+}
+
+export default ServiceDetails
+
+const styles = StyleSheet.create({
+  
+})
