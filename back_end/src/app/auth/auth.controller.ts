67e43f3e969eccc321066649118ee9d9dfb6175c
nL1x <nathan.jeannot@epitech.eu>
2025-09-30 12:19:55 +0200

    refactor(backend/auth/controller): remove double fields checks on controller

--- back_end/src/app/auth/auth.controller.ts
+++ back_end/src/app/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   HttpCode,
@@ -19,54 +18,33 @@ import type { Response } from 'express';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  private isValidEmail(email: string): boolean {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  }
-
   @HttpCode(HttpStatus.OK)
   @Post('sign-up')
   async signUp(
     @Body() signUpDto: SignUpRequestDto,
   ): Promise<SignUpResponseDto> {
-    if (
-      typeof signUpDto.email !== 'string' ||
-      !this.isValidEmail(signUpDto.email)
-    )
-      throw new BadRequestException('Invalid email format.');
-    if (typeof signUpDto.password !== 'string')
-      throw new BadRequestException('Invalid password format.');
-    if (typeof signUpDto.name !== 'string')
-      throw new BadRequestException('Invalid name format.');
-
     const { password: _, ...result } = await this.authService.signUp(signUpDto);
+
     return result;
   }
 
   @HttpCode(HttpStatus.OK)
   @Post('sign-in')
   async signIn(@Body() signInDto: SignInRequestDto, @Res() res: Response) {
-    if (
-      typeof signInDto.email !== 'string' ||
-      !this.isValidEmail(signInDto.email)
-    )
-      throw new BadRequestException('Invalid email format.');
-    if (typeof signInDto.password !== 'string')
-      throw new BadRequestException('Invalid password format.');
-
     const token = await this.authService.signIn(
       signInDto.email,
       signInDto.password,
     );
 
     res
-      .cookie('access_token', token, {
+      .cookie('session_token', token, {
         httpOnly: true,
         sameSite: 'none',
         secure: true,
       })
       .status(HttpStatus.OK)
       .json({
-        access_token: token,
+        session_token: token,
       });
   }
 }
