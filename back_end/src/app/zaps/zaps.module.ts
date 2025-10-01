9295520296daa61df36ea4cbc21b960146ecb2b9
manech <manech.dubreil@epitech.eu>
2025-10-01 22:00:00 +0200

    fix(zaps.module.ts): add miising import

--- back_end/src/app/zaps/zaps.module.ts
+++ back_end/src/app/zaps/zaps.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ZapsService } from './zaps.service';
 import { ZapsController } from './zaps.controller';
 import { PrismaService } from '@root/prisma/prisma.service';
+import { TriggersController } from './triggers/triggers.controller';
+import { TriggersModule } from './triggers/triggers.module';
 
 @Module({
-  controllers: [ZapsController],
+  controllers: [ZapsController, TriggersController],
   providers: [ZapsService, PrismaService],
+  imports: [TriggersModule],
 })
 export class ZapsModule {}
+
+// ne pas oublier de mettre l'import du trigger et des actions
