f57722ee1dfee0b3cbe2f14030d2305cfae63585
nL1x <nathan.jeannot@epitech.eu>
2025-10-19 23:15:59 +0200

    refactor(backend/runner/services/schedule): rename type CheckResult to RunnerCheckResult

--- back_end/src/runner/services/schedule/schedule.workflow.ts
+++ back_end/src/runner/services/schedule/schedule.workflow.ts
@@ -1,14 +1,15 @@
-import { CheckResult } from '@root/workflows/workflows.dto';
-import { TriggerBuilderParams } from '@root/workflows/runner/zaps/triggers/triggers.runner.factory';
-import { TriggerRunnerJob } from '@root/workflows/runner/zaps/triggers/triggers.runner.job';
+import { RunnerCheckResult, RunnerExecutionStatus } from '@root/runner/runner.dto';
+import { TriggerBuilderParams } from '@root/runner/zaps/triggers/triggers.runner.factory';
+import { TriggerRunnerJob } from '@root/runner/zaps/triggers/triggers.runner.job';
 
 export class ScheduleTrigger_EveryMinutes extends TriggerRunnerJob<object> {
   constructor(params: TriggerBuilderParams) {
     super(params);
   }
 
-  protected async _check(): Promise<CheckResult> {
+  protected async _check(): Promise<RunnerCheckResult> {
     return {
+      status: RunnerExecutionStatus.SUCCESS,
       data: [],
       is_triggered: true,
     };
