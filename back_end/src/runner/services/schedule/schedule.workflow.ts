9fe8a714006ed1bc12ed63ff3dd42fb3eb583aa0
nL1x <nathan.jeannot@epitech.eu>
2025-10-22 12:26:30 +0200

    feat(backend/services/schedule): adds schedule variables

--- back_end/src/runner/services/schedule/schedule.workflow.ts
+++ back_end/src/runner/services/schedule/schedule.workflow.ts
@@ -1,17 +1,41 @@
-import { RunnerCheckResult, RunnerExecutionStatus } from '@root/runner/runner.dto';
+import {
+  RunnerCheckResult,
+  RunnerExecutionStatus,
+} from '@root/runner/runner.dto';
 import { TriggerBuilderParams } from '@root/runner/zaps/triggers/triggers.runner.factory';
 import { TriggerRunnerJob } from '@root/runner/zaps/triggers/triggers.runner.job';
+import { ScheduleTrigger_EveryMinutes_Payload } from '@root/runner/services/schedule/schedule.dto';
 
-export class ScheduleTrigger_EveryMinutes extends TriggerRunnerJob<object> {
+export class ScheduleTrigger_EveryMinutes extends TriggerRunnerJob<ScheduleTrigger_EveryMinutes_Payload> {
   constructor(params: TriggerBuilderParams) {
     super(params);
   }
 
   protected async _check(): Promise<RunnerCheckResult> {
+    const lastExecutionTimestamp = this.lastExecution?.getTime();
+    const now = Date.now();
+    const data = [{ key: 'Date', value: new Date(now).toISOString() }];
+
+    if (!this.lastExecution)
+      return {
+        status: RunnerExecutionStatus.SUCCESS,
+        data,
+        is_triggered: true,
+      };
+
+    if (
+      lastExecutionTimestamp &&
+      now - lastExecutionTimestamp >= Number(this.payload.seconds) * 1000
+    )
+      return {
+        status: RunnerExecutionStatus.SUCCESS,
+        data,
+        is_triggered: true,
+      };
     return {
       status: RunnerExecutionStatus.SUCCESS,
       data: [],
-      is_triggered: true,
+      is_triggered: false,
     };
   }
 }
