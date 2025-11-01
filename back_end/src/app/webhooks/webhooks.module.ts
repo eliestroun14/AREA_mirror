9a6d76dd6868df198c670e6f44ec18b1dfc8f74b
nL1x <nathan.jeannot@epitech.eu>
2025-11-01 14:43:14 +0100

    hotfix(back_end/webhooks/webhook.module.ts): fix import error

--- back_end/src/app/webhooks/webhooks.module.ts
+++ back_end/src/app/webhooks/webhooks.module.ts
@@ -7,7 +7,6 @@ import { StepsService } from '@app/zaps/steps/steps.service';
 import { ConnectionsService } from '@app/users/connections/connections.service';
 import { ServicesService } from '@app/services/services.service';
 import { WebhooksService } from '@app/webhooks/webhooks.service';
-import { GithubOnNewIssueWebhookController } from '@root/services/github/triggers/on-new-issue/github-on-new-issue.controller';
 import { YoutubeNewVideoUploadWebhookController } from '@root/services/youtube/triggers/new-video-upload/youtube-new-video-upload.controller';
 import { GithubOnNewOrganisationRepositoryWebhookController } from '@root/services/github/triggers/on-new-organisation-repository/github-on-new-organisation-repository.controller';
 import { GithubOnNewRepositoryIssueWebhookController } from '@root/services/github/triggers/on-new-repository-issue/github-on-new-repository-issue.controller';
